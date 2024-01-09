a39147d4a08c4250c5ab67ca75fea6d99b2f449d
thelissimus <thelissimus@tuta.io>
2024-01-09 16:29:33 +0500

    add(interp-Lint): interpreter

--- src/Interpreter/Lint.hs
+++ src/Interpreter/Lint.hs
@@ -3,16 +3,18 @@
 module Interpreter.Lint (module Interpreter.Lint) where
 
 import Data.Kind (Type)
-import Prelude (Bool (..))
+import Prelude (Bool (..), IO, Show (show), all, error, pure, (+), (++), (-), (<$>), (<*>))
 import Prelude qualified as P
 
 type Op :: Type
 data Op = Read | Negate | Minus | Plus
+  deriving stock (Show)
 
 type Exp :: Type
 data Exp
   = Int P.Int
   | Prim Op [Exp]
+  deriving stock (Show)
 
 type Program :: Type -> Type
 data Program info = MkProgram info Exp
@@ -39,7 +41,19 @@ isExp :: Exp -> Bool
 isExp = \case
   Int _ -> True
   Prim Read _ -> True
-  Prim _ args -> P.all isExp args
+  Prim _ args -> all isExp args
 
 isLint :: Program info -> Bool
-isLint (MkProgram _ exp) = isExp exp
+isLint (MkProgram _ e) = isExp e
+
+interpExp :: Exp -> IO P.Int
+interpExp = \case
+  Int n -> pure n
+  Prim Read [] -> P.read <$> P.getLine
+  Prim Negate [a] -> P.negate <$> interpExp a
+  Prim Minus [a, b] -> (-) <$> interpExp a <*> interpExp b
+  Prim Plus [a, b] -> (+) <$> interpExp a <*> interpExp b
+  e -> error ("Invalid expression: " ++ show e)
+
+interpLint :: Program info -> IO P.Int
+interpLint (MkProgram _ e) = interpExp e
