716b85cba7c9a9ab527cbcb7a5dedc1888393042
thelissimus <thelissimus@tuta.io>
2024-01-09 15:01:06 +0500

    add(interp-Lint): predicates

--- src/Interpreter/Lint.hs
+++ src/Interpreter/Lint.hs
@@ -3,6 +3,7 @@
 module Interpreter.Lint (module Interpreter.Lint) where
 
 import Data.Kind (Type)
+import Prelude (Bool (..))
 import Prelude qualified as P
 
 type Op :: Type
@@ -27,3 +28,18 @@ minus a b = Prim Minus [a, b]
 
 plus :: Exp -> Exp -> Exp
 plus a b = Prim Plus [a, b]
+
+isLeaf :: Exp -> Bool
+isLeaf = \case
+  Int _ -> True
+  Prim Read _ -> True
+  _ -> False
+
+isExp :: Exp -> Bool
+isExp = \case
+  Int _ -> True
+  Prim Read _ -> True
+  Prim _ args -> P.all isExp args
+
+isLint :: Program info -> Bool
+isLint (MkProgram _ exp) = isExp exp
