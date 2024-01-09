a1826d8a40281c31cda7b0c239f68499b8cfded8
thelissimus <thelissimus@tuta.io>
2024-01-09 14:52:58 +0500

    add(interp-Lint): better AST and primitives

--- src/Interpreter/Lint.hs
+++ src/Interpreter/Lint.hs
@@ -5,10 +5,25 @@ module Interpreter.Lint (module Interpreter.Lint) where
 import Data.Kind (Type)
 import Prelude qualified as P
 
+type Op :: Type
+data Op = Read | Negate | Minus | Plus
+
 type Exp :: Type
 data Exp
   = Int P.Int
-  | Prim P.String [Exp]
+  | Prim Op [Exp]
 
 type Program :: Type -> Type
 data Program info = MkProgram info Exp
+
+read :: Exp
+read = Prim Read []
+
+negate :: Exp -> Exp
+negate a = Prim Negate [a]
+
+minus :: Exp -> Exp -> Exp
+minus a b = Prim Minus [a, b]
+
+plus :: Exp -> Exp -> Exp
+plus a b = Prim Plus [a, b]
