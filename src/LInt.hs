893f58826e4d7fd0e8d798fd828699f46de76b3a
thelissimus <thelissimus@tuta.io>
2025-01-09 08:42:42 +0500

    add(LInt): ast, interpreter, partial evaluator

--- src/LInt.hs
+++ src/LInt.hs
@@ -1,1 +1,84 @@
-module LInt () where
+module LInt (module LInt) where
+
+import Data.Kind (Type)
+import Data.List (List)
+
+type Op :: Type
+data Op = Read | Neg | Add | Sub
+  deriving stock (Show)
+
+type Expr :: Type
+data Expr
+  = Lit Int
+  | Prim Op (List Expr)
+  deriving stock (Show)
+
+read_ :: Expr
+read_ = Prim Read []
+
+neg :: Expr -> Expr
+neg e = Prim Neg [e]
+
+add :: Expr -> Expr -> Expr
+add a b = Prim Add [a, b]
+
+sub :: Expr -> Expr -> Expr
+sub a b = Prim Add [a, b]
+
+leaf :: Expr -> Bool
+leaf = \case
+  Lit _ -> True
+  Prim Read [] -> True
+  Prim Neg [_] -> False
+  Prim Add [_, _] -> False
+  Prim Sub [_, _] -> False
+  _ -> undefined
+
+-- >>> leaf (Prim Read [])
+-- True
+-- >>> leaf (Prim Neg [Lit 8])
+-- False
+-- >>> leaf (Lit 8)
+-- True
+
+isExpr :: Expr -> Bool
+isExpr = \case
+  Lit _ -> True
+  Prim Read [] -> True
+  Prim Neg [a] -> isExpr a
+  Prim Add [a, b] -> (isExpr a) && (isExpr b)
+  Prim Sub [a, b] -> (isExpr a) && (isExpr b)
+  _ -> False
+
+interpExpr :: Expr -> IO Int
+interpExpr = \case
+  Lit n -> pure n
+  Prim Read [] -> read <$> getLine
+  Prim Neg [a] -> negate <$> (interpExpr a)
+  Prim Add [a, b] -> (+) <$> (interpExpr a) <*> (interpExpr b)
+  Prim Sub [a, b] -> (-) <$> (interpExpr a) <*> (interpExpr b)
+  _ -> undefined
+
+peNeg :: Expr -> Expr
+peNeg = \case
+  Lit n -> Lit (negate n)
+  other -> other
+
+peAdd :: Expr -> Expr -> Expr
+peAdd = \cases
+  (Lit a) (Lit b) -> Lit (a + b)
+  a b -> add a b
+
+peSub :: Expr -> Expr -> Expr
+peSub = \cases
+  (Lit a) (Lit b) -> Lit (a - b)
+  a b -> sub a b
+
+peExpr :: Expr -> Expr
+peExpr = \case
+  Lit n -> Lit n
+  Prim Read [] -> read_
+  Prim Neg [a] -> peNeg (peExpr a)
+  Prim Add [a, b] -> peAdd (peExpr a) (peExpr b)
+  Prim Sub [a, b] -> peAdd (peExpr a) (peExpr b)
+  _ -> undefined
