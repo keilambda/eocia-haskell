158370b2c60d046b2560a3b5eefd3e35766fa931
thelissimus <thelissimus@tuta.io>
2025-01-10 18:30:35 +0500

    refactor(Pipeline): more efficient `passUniquify`
    use a hashmap that maps old to new name instead of rebinding every
    single time

--- src/Pipeline.hs
+++ src/Pipeline.hs
@@ -1,27 +1,19 @@
 module Pipeline (module Pipeline) where
 
-import Data.HashMap.Strict (delete, insert)
+import Data.HashMap.Strict (findWithDefault, insert)
 import Data.Unique (hashUnique, newUnique)
 
 import LVar qualified
 
-rebind :: String -> String -> LVar.Expr -> LVar.Expr
-rebind old new = \case
-  e@(LVar.Lit _) -> e
-  e@(LVar.Var old') -> if old == old' then LVar.Var new else e
-  e@(LVar.Let old' expr body) ->
-    if old == old'
-      then e
-      else LVar.Let old' (rebind old new expr) (rebind old new body)
-  LVar.Prim op es -> LVar.Prim op (map (rebind old new) es)
-
-passUniquify :: LVar.Env -> LVar.Expr -> IO LVar.Expr
-passUniquify env = \case
-  e@(LVar.Lit _) -> pure e
-  e@(LVar.Var _) -> pure e
-  LVar.Let name expr body -> do
-    name' <- (\n -> name ++ "." ++ show n) . hashUnique <$> newUnique
-    expr' <- passUniquify env expr
-    body' <- passUniquify (insert name' expr (delete name env)) (rebind name name' body)
-    pure $ LVar.Let name' expr' body'
-  LVar.Prim op es -> LVar.Prim op <$> traverse (passUniquify env) es
+passUniquify :: LVar.Expr -> IO LVar.Expr
+passUniquify = loop mempty
+ where
+  loop env = \case
+    e@(LVar.Lit _) -> pure e
+    LVar.Var n -> pure $ LVar.Var (findWithDefault n n env)
+    LVar.Let name expr body -> do
+      name' <- (\n -> name ++ "." ++ show n) . hashUnique <$> newUnique
+      expr' <- loop env expr
+      body' <- loop (insert name name' env) body
+      pure $ LVar.Let name' expr' body'
+    LVar.Prim op es -> LVar.Prim op <$> traverse (loop env) es
