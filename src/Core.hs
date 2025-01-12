3ff4329d2d641430c4dbec1c91d864f9ece22f8e
thelissimus <thelissimus@tuta.io>
2025-01-13 02:39:16 +0500

    add(Core): `IsString` instances for `Name` and `Label`

--- src/Core.hs
+++ src/Core.hs
@@ -8,6 +8,7 @@ where
 
 import Data.Hashable (Hashable)
 import Data.Kind (Type)
+import Data.String (IsString)
 import Data.Text (Text)
 
 import Prettyprinter (Pretty (pretty), defaultLayoutOptions, layoutPretty)
@@ -18,7 +19,7 @@ import Core.Gensym
 type Name :: Type
 newtype Name = MkName {getName :: Text}
   deriving stock (Show, Eq)
-  deriving newtype (Hashable)
+  deriving newtype (Hashable, IsString)
 
 instance Pretty Name where
   pretty (MkName t) = pretty t
@@ -26,7 +27,7 @@ instance Pretty Name where
 type Label :: Type
 newtype Label = MkLabel {getLabel :: Text}
   deriving stock (Show, Eq)
-  deriving newtype (Hashable)
+  deriving newtype (Hashable, IsString)
 
 instance Pretty Label where
   pretty (MkLabel t) = pretty t
