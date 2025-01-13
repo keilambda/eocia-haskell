54643a65a1593efcb9983bc29a71877c838c63c9
thelissimus <thelissimus@tuta.io>
2025-01-13 06:41:20 +0500

    add(TestPipeline): pass uniquify property and unit tests

--- test/Main.hs
+++ test/Main.hs
@@ -5,9 +5,10 @@ import Test.Tasty (TestTree, defaultMain, testGroup)
 import TestCVar qualified
 import TestLInt qualified
 import TestLVar qualified
+import TestPipeline qualified
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [TestLInt.tests, TestLVar.tests, TestCVar.tests]
+tests = testGroup "Tests" [TestLInt.tests, TestLVar.tests, TestCVar.tests, TestPipeline.tests]
---
+++ test/TestPipeline.hs
@@ -0,0 +1,37 @@
+module TestPipeline (tests) where
+
+import Control.Monad.State (evalState)
+
+import Data.HashMap.Strict (HashMap, insertWith, unionWith)
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+import Core (Name, renderText)
+import Pipeline (passUniquify)
+import Stage.LVar qualified as LVar
+import TestLVar ()
+
+tests :: TestTree
+tests = testGroup "Pipeline" [groupPassUniquify]
+
+countVars :: LVar.Expr -> HashMap Name Int
+countVars = \case
+  LVar.Lit _ -> mempty
+  LVar.Var _ -> mempty
+  LVar.Let name expr body -> insertWith (+) name 1 (unionWith (+) (countVars expr) (countVars body))
+  LVar.Prim _ es -> foldMap countVars es
+
+groupPassUniquify :: TestTree
+groupPassUniquify =
+  testGroup
+    "passUniquify"
+    [ testProperty "variable is bound at most once" $ property \expr ->
+        let vars = countVars (evalState (passUniquify expr) (0 :: Int))
+         in all (<= 1) vars
+    , testCase "preserves semantics" $
+        let expr = LVar.Let "x" (LVar.Lit 1) (LVar.Let "x" (LVar.Var "x") (LVar.Var "x"))
+            expr' = evalState (passUniquify expr) (0 :: Int)
+         in renderText expr' @?= "(let [x.0 1] (let [x.1 x.0] x.1))"
+    ]
