95fbcbb339736e7c330cafd213aed4cd2a78f76b
thelissimus <thelissimus@tuta.io>
2025-01-26 15:46:13 +0500

    add(test): CVar Arbitrary instances

--- test/Arbitrary.hs
+++ test/Arbitrary.hs
@@ -5,6 +5,8 @@ import Data.Text (pack)
 import Test.Tasty.QuickCheck
 
 import Core (Label (MkLabel), Name (MkName))
+import Stage.CVar qualified as CVar
+import Stage.LInt (BinOp (Add, Sub), NulOp (Read), UnOp (Neg))
 import Stage.LInt qualified as LInt
 import Stage.LVar qualified as LVar
 
@@ -49,4 +51,32 @@ instance Arbitrary LVar.Expr where
               a <- resize (n `div` 2) arbitrary
               b <- resize (n `div` 2) arbitrary
               elements [LVar.add a b, LVar.sub a b]
+          ]
+
+instance Arbitrary CVar.Atom where
+  arbitrary = oneof [CVar.Lit <$> arbitrary, CVar.Var <$> arbitrary]
+
+instance Arbitrary CVar.Expr where
+  arbitrary = sized \n ->
+    if n <= 0
+      then CVar.Atom <$> arbitrary
+      else
+        oneof
+          [ CVar.Atom <$> arbitrary
+          , pure (CVar.NulApp Read)
+          , CVar.UnApp Neg <$> arbitrary
+          , CVar.BinApp <$> elements [Add, Sub] <*> arbitrary <*> arbitrary
+          ]
+
+instance Arbitrary CVar.Stmt where
+  arbitrary = CVar.Assign <$> arbitrary <*> arbitrary
+
+instance Arbitrary CVar.Tail where
+  arbitrary = sized \n ->
+    if n <= 0
+      then CVar.Return <$> arbitrary
+      else
+        frequency
+          [ (1, CVar.Return <$> arbitrary)
+          , (4, CVar.Seq <$> arbitrary <*> resize (n `div` 2) arbitrary)
           ]
